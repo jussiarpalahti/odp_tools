d0655cfae6aa8134b8c91486af1a8aac96b54def
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2017-04-16 20:25:33 +0300

    Hopefully more node compatible now

--- src/convert.ts
+++ src/convert.ts
@@ -2,10 +2,17 @@
 import {NewDataset} from './new';
 import {OriginalDataset} from './old';
 
+import * as _ from 'lodash';
 import * as fs from "fs";
 
-function Convert() {
+function convert() {
 
-    let doc = <OriginalDataset> JSON.parse(fs.readFileSync('../one_package.json', 'utf8'));
-
+    console.log("converting");
+    let doc = <OriginalDataset> JSON.parse(
+        fs.readFileSync('one_package.json', 'utf8'));
+    let template = <OriginalDataset> JSON.parse(
+        fs.readFileSync('template.json', 'utf8'));
+    console.log("don't");
 }
+
+convert();
