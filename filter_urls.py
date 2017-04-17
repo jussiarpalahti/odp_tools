5e236efdc77c830be544df0e0e8a5081aefa71a3
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2017-04-17 22:02:48 +0300

    Groups and tags from names to dicts
    Importer wants these as list of dicts, not list of names as source data has

--- filter_urls.py
+++ filter_urls.py
@@ -34,5 +34,93 @@ def get_organizations():
 
 	json.dump(ok_orgs, open("orgs.json",'w'), indent=2)
 
-get_resources()
-get_organizations()
+# get_resources()
+# get_organizations()
+
+
+def add_tags():
+
+	from ckanapi import RemoteCKAN
+	ua = 'ckanapiexample/1.0 (+http://hri.fi)'
+	
+	c = RemoteCKAN('http://hri.fi',user_agent=ua)
+
+	tags = c.action.tag_list(all_fields=True)
+	tags_data = dict(((g['name'], g) for g in tags))
+
+	new_hri_docs = [json.loads(doc) for doc in open('out.json').read().split("\n")]
+
+	yes_tags = set()
+	no_tags = set()
+	
+	for doc in new_hri_docs:
+	
+		doc_tags = []
+	
+		for doc_group in doc.get('tags', []):
+	
+			gdata = tags_data.get(doc_group)
+	
+			if gdata:
+				doc_tags.append(gdata)
+				yes_tags.add(doc_group)
+			else:
+				no_tags.add(doc_group)
+	
+		if doc_tags:
+			doc['tags'] = doc_tags
+
+		elif doc.get('tags'):
+			del doc['tags']
+
+	print("no group ", no_tags)
+	print("----------------------")
+	print("yes group ", yes_tags)
+
+	result = [json.dumps(doc) for doc in new_hri_docs]
+	open('out_w_tags.json', 'w').write('\n'.join(result))
+
+add_tags()
+
+
+def add_groups():
+
+	from ckanapi import RemoteCKAN
+	ua = 'ckanapiexample/1.0 (+http://hri.fi)'
+	
+	c = RemoteCKAN('http://hri.fi',user_agent=ua)
+	
+	groups = c.action.group_list(all_fields=True)
+	groups_data = dict(((g['name'], g) for g in groups))
+
+	new_hri_docs = [json.loads(doc) for doc in open('out.json').read().split("\n")]
+
+	yes_groups = set()
+	no_groups = set()
+	
+	for doc in new_hri_docs:
+	
+		doc_groups = []
+	
+		for doc_group in doc.get('groups', []):
+	
+			gdata = groups_data.get(doc_group)
+	
+			if gdata:
+				doc_groups.append(gdata)
+				yes_groups.add(doc_group)
+			else:
+				no_groups.add(doc_group)
+	
+		if doc_groups:
+			doc['groups'] = doc_groups
+
+		elif doc.get('groups'):
+			del doc['groups']
+
+	print("no group ", no_groups)
+	print("----------------------")
+	print("yes group ", yes_groups)
+
+	result = [json.dumps(doc) for doc in new_hri_docs]
+	open('out_w_groups.json', 'w').write('\n'.join(result))
